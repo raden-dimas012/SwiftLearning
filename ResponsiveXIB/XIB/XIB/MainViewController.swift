30e75e5dbd71d259b4643a16c228b7d7f6369a4c
raden-dimas012 <dimascode012@gmail.com>
2023-02-05 15:59:20 +0700

    add responsiveXIB & Scrollview XIB

---
+++ ResponsiveXIB/XIB/XIB/MainViewController.swift
@@ -0,0 +1,17 @@
+//
+//  MainViewController.swift
+//  XIB
+//
+//  Created by Raden Dimas on 24/01/23.
+//
+
+import UIKit
+
+final class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+
+}
---
+++ ScrollView/ScrollViewXIB/ScrollViewXIB/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  ScrollViewXIB
+//
+//  Created by Raden Dimas on 05/02/23.
+//
+
+import UIKit
+
+final class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
